e3f85c22afdd09a56ea26d460c90e7b09fd36ee3
Per Thomsen <PerThoms1@proton.me>
2026-01-12 13:21:19 +0100

    pls_config moved and corrected

---
+++ src/config/psl-config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "mariadb6.netsite.dk");     // The host you want to connect to.
+define("USER", "monacorinfo");    // The database username. 
+define("PASSWORD", "kK31Hz2u$9");    // The database password.
+//define("PASSWORD", "4Fa98xkHVd2XmnfK");    // The database password.
+ 
+define("DATABASE", "monacorinfo");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
