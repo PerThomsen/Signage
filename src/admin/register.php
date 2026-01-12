47b4814dfd39bb9d6a5d11f955e600b84ed2663c
Per Thomsen <PerThoms1@proton.me>
2026-01-12 13:20:33 +0100

    Register moved and corrected

--- src/admin/register.php
+++ src/admin/register.php
@@ -1,35 +1,20 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors','On');
-require_once("config/init.php");
-include_once 'inc/functions.php';
-include_once 'inc/register.inc.php';
-include_once 'inc/selectCountryCode.inc.php';
+require_once("../config/init.php");
+include_once $sti_til_fil.'/config/register.inc.php';
+
 sec_session_start();
 
-$mysqli = new mysqli('localhost', 'monacor', 'mona5800', 'media');
-if(login_check($mysqli) == true) {
-  if (isset($_SESSION['editMenu']) && ($_SESSION['editMenu'] == '1')) {  		
-    $menuEdit = 1;
-  } else {
-    $menuEdit = 0;
-  } 
-  if (isset($_SESSION['admin']) && ($_SESSION['admin'] == '1')) {
-    $admShow = 1;
-  } else {
-    $admShow = 0;
-  }
+//$mysqli = new mysqli('localhost', 'monacor', 'mona5800', 'media');
 
+if(login_check($mysqli) == true) {
+ 
 $usrListeTxt = "";
 $sql = <<<SQLTXT
   SELECT
     `username`,
-    `email`,
-    `languageId`,
-    `changeLanguage`,
-    `admin`,
-    `editMenu`,
-    `editArtikel`
+    `email`
   FROM `1_members`
 SQLTXT;
 
@@ -40,19 +25,9 @@ if( $database->num_rows( $sql ) > 0 ) {
   foreach( $results as $row ) {
 	  $usr	  = $row["username"];
 	  $ml 	  = $row["email"];
-	  $lng	  = $row["languageId"];
-	  $cl 	  = $row["changeLanguage"];
-	  $adm 	  = $row["admin"];
-	  $mnu 	  = $row["editMenu"];
-	  $art 	  = $row["editArtikel"];
     $usrListeTxt .= "<tr>";
     $usrListeTxt .= "<td>$usr</td>";
     $usrListeTxt .= "<td>$ml</td>";
-    $usrListeTxt .= "<td>$lng</td>";
-    $usrListeTxt .= "<td>$cl</td>";
-    $usrListeTxt .= "<td>$adm</td>";
-    $usrListeTxt .= "<td>$mnu</td>";
-    $usrListeTxt .= "<td>$art</td>";
     $usrListeTxt .= "</tr>";
   }
   $usrListeTxt .= "</tbody>";  
