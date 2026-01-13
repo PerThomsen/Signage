a97c753f9ad6d4d862645941f924d16c097cfde3
Per Thomsen <PerThoms1@proton.me>
2026-01-13 15:37:53 +0100

    Update start_mid.php

--- src/admin/inc/start_mid.php
+++ src/admin/inc/start_mid.php
@@ -1,10 +1,10 @@
 <div class="container">
-	<h1>Redigeringsside</h1>
+	<h1>Backend</h1>
 	<div class="row">
 	  <div class="col-lg-12 mb-4 mx-auto">
 	    <div class="card border border-primary mon_border_style p-5">
 	      <div class="card-body">
-	        <div class="box1"><?php include_once '../inc/date.php';//Dynamisk kalender (kun hvis udfyldt>?></div>
+	        <div class="box1"><?php include_once 'inc/date.php';//Dynamisk kalender (kun hvis udfyldt>?></div>
 	        <p>&nbsp</p>
 	        <div class="box3"><?php echo "Besøg"//Besøg af (kun hvis udfyldt> ?></div>
 	        <p>&nbsp</p>
