22a7e0fb33aa8d50caca0fe89641f5e6f21fc11c
Per Thomsen <PerThoms1@proton.me>
2026-01-13 15:38:02 +0100

    Update date.php

--- src/admin/inc/date.php
+++ src/admin/inc/date.php
@@ -7,7 +7,7 @@ $currentYear = $date->format('Y');
 $numDays = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
 ?>
 <table>
-  <tr>'
+  <tr>
     <th>Sun</th>
     <th>Mon</th>
     <th>Tue</th>
@@ -18,23 +18,6 @@ $numDays = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
   </tr>
 
 <?php
-$table = $GLOBALS['cTable'];
-$sql = <<<SQLTXT
-  SELECT
-    `cat_id`,
-    `cat_txt`
-  FROM `$table`
-SQLTXT;
-
-$cat = array();
-if( $database->num_rows( $sql ) > 0 ) {
-  $results = $database->get_results( $sql );
-  foreach( $results as $row ) {
-    $cId    = $row["cat_id"];
-    $cTxt   = $row["cat_txt"];
-    $cat[$cId] = $cTxt;
-  }
-}
 
 $table = $GLOBALS['dTable'];
 $sql = <<<SQLTXT
@@ -47,22 +30,70 @@ $sql = <<<SQLTXT
     `body_text` 
   FROM `$table`
   WHERE `category_id` = 1
+  AND EXTRACT(month FROM (due_date)) = $currentMonth
 SQLTXT;
 
+$month   = array();
+if( $database->num_rows( $sql ) > 0 ) {
+  $results = $database->get_results( $sql );
+  foreach( $results as $row ) {
+    $id    = $row["id"];
+    $due   = $row["due_date"];
+    /*
+    $month[] = array($id, array(
+      
+      "due" => $due, 
+      "cat" => $cat,
+      "hdr" => $hdr,
+      "bdy" => $bdy,
+      "day" => date("d", strtotime($due)),
+      "mth" => date("m", strtotime($due)),
+      "yr"  => date("Y", strtotime($due))
+    ));
+    */
+    $month[] = date("d", strtotime($due));
+
+    //echo $month[$id]["day"].'<br />';
+  }
+  
+ 
+}
+//*
+echo '<pre>';
+print_r($month);
+echo '</pre>';
+//*/
+
+//echo $month[0]["day"]."<br />";
+//echo $month[1]["day"]."<br />";
+$cal_data = '';
 for ($i = 1; $i <= $numDays; $i++) {
   $dayOfWeek = date('w', strtotime("$currentYear-$currentMonth-$i"));
   if ($i == 1) {
-    echo '<tr>';
+    $cal_data .= '<tr>';
+    //echo '<tr>';
     for ($j = 1; $j <= $dayOfWeek; $j++) {
-      echo '<td></td>';
+      $cal_data .=  '<td></td>';
     }
   }
-  echo "<td>$i</td>";
+ 
+  $length = count($month);
+  for ($k = 0; $k < $length; $k++) { 
+    if (isset($month[$k]["day"]) && $month[$k]["day"] == $i) {
+      //$dato = '<strong>'.$i.'</strong>';
+      $cal_data .= "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'><strong>'.$i.'</strong></a></td>";
+    } else {
+      //$dato = $i;
+      $cal_data .= "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'>'.$i.'</a></td>";
+    }
+  } 
+  
+  //echo "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'>$dato</a></td>";
   if ($dayOfWeek == 6 || $i == $numDays) {
-    echo '</tr>';
+    $cal_data .=  '</tr>';
   }
 }
-
+echo $cal_data;
 ?>
 
 </table>
