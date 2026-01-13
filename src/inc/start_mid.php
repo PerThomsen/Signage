9b3186078cc3d2a61fb840be5d3288b7dd7d0eeb
Per Thomsen <PerThoms1@proton.me>
2026-01-13 08:48:21 +0100

    Update start_mid.php

--- src/inc/start_mid.php
+++ src/inc/start_mid.php
@@ -1,3 +1,15 @@
-  <div class="container">
-  	<h1>Redigeringsside</h1>
-  </div>
+<div class="container">
+	<h1>Redigeringsside</h1>
+	<div class="row">
+	  <div class="col-lg-12 mb-4 mx-auto">
+	    <div class="card border border-primary mon_border_style p-5">
+	      <div class="card-body">
+	        <div class="box1"><?php include_once '../inc/date.php';//Dynamisk kalender (kun hvis udfyldt>?></div>
+	        <p>&nbsp</p>
+	        <div class="box3">Besøg af (kun hvis udfyldt></div>
+	        <p>&nbsp</p>
+	        <div class="box4">Vi arbejder hjemme i dag (kun hvis aktiv></div>
+	        <p>&nbsp</p>
+	    </div>
+	</div>
+</div>
