1761dc99aa211c904b0c698d16e54664e2183534
Per Thomsen <PerThoms1@proton.me>
2026-01-13 15:41:52 +0100

    Update date.php

--- src/admin/inc/date.php
+++ src/admin/inc/date.php
@@ -22,12 +22,7 @@ $numDays = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
 $table = $GLOBALS['dTable'];
 $sql = <<<SQLTXT
   SELECT 
-    `id`, 
-    `creation_time`, 
-    `due_date`, 
-    `category_id`, 
-    `headertxt`, 
-    `body_text` 
+    `due_date`
   FROM `$table`
   WHERE `category_id` = 1
   AND EXTRACT(month FROM (due_date)) = $currentMonth
@@ -37,35 +32,11 @@ $month   = array();
 if( $database->num_rows( $sql ) > 0 ) {
   $results = $database->get_results( $sql );
   foreach( $results as $row ) {
-    $id    = $row["id"];
     $due   = $row["due_date"];
-    /*
-    $month[] = array($id, array(
-      
-      "due" => $due, 
-      "cat" => $cat,
-      "hdr" => $hdr,
-      "bdy" => $bdy,
-      "day" => date("d", strtotime($due)),
-      "mth" => date("m", strtotime($due)),
-      "yr"  => date("Y", strtotime($due))
-    ));
-    */
-    $month[] = date("d", strtotime($due));
-
-    //echo $month[$id]["day"].'<br />';
+    $days[] = date("d", strtotime($due));
   }
-  
- 
 }
-//*
-echo '<pre>';
-print_r($month);
-echo '</pre>';
-//*/
 
-//echo $month[0]["day"]."<br />";
-//echo $month[1]["day"]."<br />";
 $cal_data = '';
 for ($i = 1; $i <= $numDays; $i++) {
   $dayOfWeek = date('w', strtotime("$currentYear-$currentMonth-$i"));
@@ -77,18 +48,12 @@ for ($i = 1; $i <= $numDays; $i++) {
     }
   }
  
-  $length = count($month);
-  for ($k = 0; $k < $length; $k++) { 
-    if (isset($month[$k]["day"]) && $month[$k]["day"] == $i) {
-      //$dato = '<strong>'.$i.'</strong>';
-      $cal_data .= "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'><strong>'.$i.'</strong></a></td>";
-    } else {
-      //$dato = $i;
-      $cal_data .= "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'>'.$i.'</a></td>";
-    }
-  } 
-  
-  //echo "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'>$dato</a></td>";
+  if (in_array($i, $days)) {
+    $cal_data .= "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'><strong>$i</strong></a></td>";
+  } else {
+    $cal_data .= "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'>$i</a></td>";
+  }
+
   if ($dayOfWeek == 6 || $i == $numDays) {
     $cal_data .=  '</tr>';
   }
