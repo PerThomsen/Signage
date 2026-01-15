ef4bd946a79f350a479e1bcf287edcb0f075a8d2
Per Thomsen <PerThoms1@proton.me>
2026-01-15 08:36:59 +0100

    date_edit -> data_edit

--- src/admin/inc/date.php
+++ src/admin/inc/date.php
@@ -5,6 +5,7 @@ $date = new DateTime();
 $currentMonth = $date->format('m');
 $currentYear = $date->format('Y');
 $numDays = cal_days_in_month(CAL_GREGORIAN, $currentMonth, $currentYear);
+$filNavn = 'inc/data_edit.php';
 ?>
 <table>
   <tr>
@@ -49,9 +50,9 @@ for ($i = 1; $i <= $numDays; $i++) {
   }
  
   if (in_array($i, $days)) {
-    $cal_data .= "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'><strong>$i</strong></a></td>";
+    $cal_data .= "<td><a href='$filnavn?d=$i&m=$currentMonth&y=$currentYear'><strong>$i</strong></a></td>";
   } else {
-    $cal_data .= "<td><a href='inc/date_edit.php?d=$i&m=$currentMonth&y=$currentYear'>$i</a></td>";
+    $cal_data .= "<td><a href='$filnavn?d=$i&m=$currentMonth&y=$currentYear'>$i</a></td>";
   }
 
   if ($dayOfWeek == 6 || $i == $numDays) {
