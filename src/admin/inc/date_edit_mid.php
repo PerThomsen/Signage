8c33b06ffdc0127c164aaf5e1af3cdd14f47e2b7
Per Thomsen <PerThoms1@proton.me>
2026-01-16 14:11:34 +0100

    Update date_edit_mid.php

--- src/admin/inc/date_edit_mid.php
+++ src/admin/inc/date_edit_mid.php
@@ -22,12 +22,21 @@
     <div class="col-lg-12 mb-4 mx-auto">
       <div class="card border border-primary mon_border_style p-5">
         <div class="card-header">
-          <?php echo $catTxt; ?>
+          <?php echo $catTxt." - ".$dayNice; ?>
         </div>
         <div class="card-body">
           <form name="Edit" role="form" onSubmit="return formCheck()" action="<?php echo $updateFile; ?>">
-            <input type="text" class="form-control" name="headerTxt" value="<?php echo $headerTxt; ?>">
-            <textarea class="ckeditor" cols="140" id="editor1" name="bodyTxt" rows="15">
+              <div class="form-group form-group-sm" data-toggle="tooltip" title="Key product" >
+              <label <?php echo $homeWork?'':'hidden'; ?> class="col-sm-5 control-label" for="lock">
+                Enable
+              </label>
+              <div class="col-sm-7">
+                <input <?php echo $homeWork?'':'hidden'; ?> type="checkbox" name="homeWork" value="yes" <?php echo (($homeWork==1)?'checked':''); ?> />
+              </div>
+            </div>
+
+            <input <?php echo $homeWork?'hidden':''; ?> type="text" class="form-control" name="headerTxt" value="<?php echo $headerTxt; ?>">
+            <textarea <?php echo $homeWork?'disabled':''; ?> class="ckeditor" cols="140" id="editor1" name="bodyTxt" rows="15">
               <?php echo $bodyTxt; ?>
             </textarea>
             <input type="hidden" name="datId" value="<?php echo $datId; ?>" />
@@ -36,7 +45,7 @@
             <input type="hidden" name="date" value="<?php echo $dateTxt; ?>" />
             <button name="Upd" type="submit" class="btn btn-primary">Save changes</button>
             <input class="btn btn-primary" type="button" name="SubmitCancel" value="Cancel" onclick="cancelRedirect('start.php');" /> 
-            <a href="#" class="btn btn-danger" role="button">Delete</a>
+            <a href="<?php echo $updateFile.'?delete=1&datId='.$datId.'&catId='.$cat; ?>" class="btn btn-danger" role="button">Delete</a>
           </form>
           <p>&nbsp</p>
         </div>
