d22c11648bb6ec5658a569e9494dcfa3f05cde1a
Per Thomsen <PerThoms1@proton.me>
2026-01-16 09:33:18 +0100

    Update start_mid.php

--- src/admin/inc/start_mid.php
+++ src/admin/inc/start_mid.php
@@ -1,15 +1,20 @@
 <div class="container">
-	<h1>Backend</h1>
+	<h1>Backend - <?php echo $grpTxtHdr; ?></h1>
 	<div class="row">
 	  <div class="col-lg-12 mb-4 mx-auto">
 	    <div class="card border border-primary mon_border_style p-5">
 	      <div class="card-body">
 	        <div class="box1"><?php include_once 'inc/calendar.php';//Dynamisk kalender (kun hvis udfyldt>?></div>
 	        <p>&nbsp</p>
-	        <div class="box3"><?php echo "Besøg"//Besøg af (kun hvis udfyldt> ?></div>
-	        <p>&nbsp</p>
-	        <div class="box4"><?php echo "Hjemme"//Vi arbejder hjemme i dag (kun hvis aktiv>?></div>
-	        <p>&nbsp</p>
+          	<select class='form-control' name='languageId' id='languageId' onchange='getSelVal(this);' class='languageId'>     
+      			<?php include 'inc/selector_grp.php'; ?>
+      		</select>      
+      		<script type="text/javascript">
+        		function getSelVal(sel) {
+           		var url = '<?php echo $_SERVER['PHP_SELF']; ?>' 
+           		location.href=url+'?cat='+sel.value;
+        		}
+      		</script>
 	    </div>
 	</div>
 </div>
