056d61f33ca125fc1b9252b9dd04e1c010acf70f
Per Thomsen <PerThoms1@proton.me>
2026-01-16 09:33:26 +0100

    Update start.php

--- src/admin/start.php
+++ src/admin/start.php
@@ -4,19 +4,33 @@
   require_once("../config/init.php");
   include_once '../config/functions.php';
   sec_session_start();
+  $grpTxtHdr = 'Vælg emne';
 
 /*
   echo '<pre>';
   print_r($_REQUEST);
   print_r($_SESSION);
   echo '</pre>';
-*/
+/*/
 
   if (!isset($_SESSION['login_string'])) {
     header("Location: index.php");
   }
 
-    
+  if (isset($_REQUEST['cat'])) {
+    $grpId    = $_REQUEST['cat'];
+  } else {
+    $grpId = 1;
+  }
+  $grp_file = $GLOBALS['cTable'];
+  $selector = '';
+  $query    = "SELECT cat_txt FROM `$grp_file` WHERE cat_id = $grpId;";
+  $results  = $database->get_results( $query );
+
+  if ( $database->num_rows( $query ) > 0 ) {
+    list( $grpTxtHdr) = $database->get_row( $query );
+  }
+
   $title    = "Monacor Split";
   $pageName = "Backend - Edit";
   $cFirma   = "Monacor International";
