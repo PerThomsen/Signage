7893fe6c453d918d31b85f9bc6b31f2305203ed0
Per Thomsen <PerThoms1@proton.me>
2026-01-16 12:32:27 +0100

    Update html_top.php

--- src/admin/inc/html_top.php
+++ src/admin/inc/html_top.php
@@ -14,7 +14,7 @@
     <!-- Bootstrap core CSS -->                            
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/split/css/font-awesome.min.css">
-    <link href="/split/css/pt_style.css" rel="stylesheet" type="text/css">
+    <link href="/split/css/monacor.css" rel="stylesheet" type="text/css">
     
     <script src="/split/js/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
