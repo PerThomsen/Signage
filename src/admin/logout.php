e480a71db16ab1642a5bcae590fde78bc5279dcb
Per Thomsen <PerThoms1@proton.me>
2026-01-20 13:37:45 +0100

    Update logout.php

--- src/admin/logout.php
+++ src/admin/logout.php
@@ -1,11 +1,10 @@
 <?php
-include_once 'functions.php';
-//include_once 'includes/functions.php';
+require_once("../config/init.php");
+
+//include_once '../config/functions.php';
+
 sec_session_start();
  
-if (isset($_SESSION['languagePrefx'])){
-  $langPre = $_SESSION['languagePrefx'];
-}
 // Unset all session values 
 $_SESSION = array();
  
@@ -13,18 +12,16 @@ $_SESSION = array();
 $params = session_get_cookie_params();
  
 // Delete the actual cookie. 
+
 setcookie(session_name(),
         '', time() - 42000, 
         $params["path"], 
         $params["domain"], 
         $params["secure"], 
         $params["httponly"]);
- 
+
 // Destroy session 
 session_destroy();
-if (($langPre == 'dk') || ($langPre == 'no')){
-  header('Location: ../index.htm');
-} else {
-  header('Location: ../login.php');
-}
+header('Location: index.php');
+
 ?>
